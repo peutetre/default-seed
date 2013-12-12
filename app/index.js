ac5eee2540c9cc743d2960a23c7c9cff967376f1
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-12-12 15:48:50 +0100

    wip

---
+++ app/index.js
@@ -0,0 +1,40 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+var DefaultSeedGenerator = module.exports = function DefaultSeedGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({
+        npm: true,
+        bower: false
+    });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(DefaultSeedGenerator, yeoman.generators.Base);
+
+DefaultSeedGenerator.prototype.askUserFor = function askFor() {
+  var cb = this.async();
+
+  var prompts = [{
+    type:'input',
+    name:'name',
+    message:'Give me a name ?',
+    default:null
+  }];
+
+  this.prompt(prompts, function (props) {
+    this.name = props.name;
+  }.bind(this));
+
+};
+
+MobileAppGenerator.prototype.app = function app() {
+  this.log.ok('YEP ' + this.name);
+  this.copy('index.html', 'www/index.html');
+};
