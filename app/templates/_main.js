7f26fcf54d1207fb92e64c6140ec0a32b39b41d0
Paul Panserrieu <paul.panserrieu@gmail.com>
2013-12-23 16:02:23 +0100

    wip

--- app/templates/_main.js
+++ app/templates/_main.js
@@ -1,1 +1,9 @@
 /* main.js - main entry point from <%= appName %> */
+
+'use strict';
+
+function main() {
+    alert('app <%= appName %> is ready!');
+}
+
+window.document.addEventListener(window.Codova ? 'deviceready' : 'DOMContentLoaded', main, false);
